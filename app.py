3ac29a69e6faa866ba17b637220832b531b175c9
VRINDA ASOKAN <vrinda.asokan@gmail.com>
2019-04-25 14:20:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -14,42 +14,15 @@ def qgen():
        con.row_factory = sql.Row
        cur = con.cursor()
        if(code == '1'):
-             cur.execute("SELECT * FROM questions WHERE unum = '1'")
+             cur.execute("SELECT * FROM questions WHERE unum IN ('1', '3')")
              rows = cur.fetchall() 
        else:
-             cur.execute("SELECT * FROM questions WHERE unum = '2'")
+             cur.execute("SELECT * FROM questions WHERE unum IN ('2', '4')")
              rows = cur.fetchall() 
 
        
        return render_template("qgen.html", rows = rows)
 
-
-'''@app.route('/enternew')
-def new_student():
-   return render_template('student1.html')
-
-@app.route('/addrec',methods = ['POST', 'GET'])
-def addrec():
-   if request.method == 'POST':
-      try:
-         nm = request.form['nm']
-         addr = request.form['add']
-         city = request.form['city']
-         pin = request.form['pin']
-         
-         with sql.connect("database.db") as con:
-            cur = con.cursor()
-            cur.execute("INSERT INTO students (name,addr,city,pin) VALUES (?,?,?,?)",(nm,addr,city,pin) )
-            
-            con.commit()
-            msg = "Record successfully added"
-      except:
-         con.rollback()
-         msg = "error in insert operation"
-      
-      finally:
-         return render_template("result1.html",msg = msg)
-         con.close()'''
 
 if __name__ == '__main__':
    app.run(debug = True)
