bdc88270a4302d8534c004a708597c14f46d6c38
VRINDA ASOKAN <vrinda.asokan@gmail.com>
2019-05-07 18:57:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -15,13 +15,25 @@ def qgen():
        cur = con.cursor()
        if(code == '1'):
              cur.execute("SELECT * FROM questions WHERE unum IN ('1', '3')")
-             rows = cur.fetchall() 
+             rows = cur.fetchall()
+             list_qn = []
+             list_qno = []
+             list_ans = []
+             for row in rows:
+                   list_qno.append(row["unum"])
+                   list_qn.append(row["ques"])
+                   list_ans.append(row["ans"])
+             print(list_qno)
+             print(list_qn)
+             print(list_ans)
+             return render_template("qgen.html", rows = rows) 
        else:
              cur.execute("SELECT * FROM questions WHERE unum IN ('2', '4')")
-             rows = cur.fetchall() 
+             rows = cur.fetchall()
+             return render_template("qgen1.html", rows = rows) 
 
        
-       return render_template("qgen.html", rows = rows)
+       '''return render_template("qgen.html", rows = rows)'''
 
 
 if __name__ == '__main__':
